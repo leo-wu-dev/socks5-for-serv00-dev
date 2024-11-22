fbbf8ace3dfe51addd754311644e4cb8ee2ddd86
leo-wu-dev <leo.wu.email@gmail.com>
2024-11-22 08:58:02 +0800

    Update check_cron-dev.sh

--- check_cron-dev.sh
+++ check_cron-dev.sh
@@ -11,11 +11,11 @@ REBOOT_COMMAND="@reboot pkill -kill -u $(whoami) && $PM2_PATH resurrect >> /home
 
 echo "检查并添加 crontab 任务"
 
-if [ "$(command -v pm2)" == "/home/${USER}/.npm-global/bin/pm2" ]; then
-  echo "已安装 pm2，并返回正确路径，启用 pm2 保活任务"
-  (crontab -l | grep -F "$REBOOT_COMMAND") || (crontab -l; echo "$REBOOT_COMMAND") | crontab -
-  (crontab -l | grep -F "$CRON_JOB") || (crontab -l; echo "$CRON_JOB") | crontab -
-else
+# if [ "$(command -v pm2)" == "/home/${USER}/.npm-global/bin/pm2" ]; then
+#   echo "已安装 pm2，并返回正确路径，启用 pm2 保活任务"
+#   (crontab -l | grep -F "$REBOOT_COMMAND") || (crontab -l; echo "$REBOOT_COMMAND") | crontab -
+#   (crontab -l | grep -F "$CRON_JOB") || (crontab -l; echo "$CRON_JOB") | crontab -
+# else
   if [ -e "${WORKDIR}/start.sh" ] && [ -e "${FILE_PATH}/config.json" ]; then
     echo "添加 nezha & socks5 的 crontab 重启任务"
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5} && ${CRON_NEZHA}") | crontab -
@@ -30,4 +30,4 @@ else
     (crontab -l | grep -F "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") || (crontab -l; echo "@reboot pkill -kill -u $(whoami) && ${CRON_S5}") | crontab -
     (crontab -l | grep -F "* * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") || (crontab -l; echo "*/12 * * * * pgrep -x \"s5\" > /dev/null || ${CRON_S5}") | crontab -
   fi
-fi
+# fi
