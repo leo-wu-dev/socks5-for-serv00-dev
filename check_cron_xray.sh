7965ddd7ed88624944c51012a47d5a567cc00d47
leo-wu-dev <leo.wu.email@gmail.com>
2024-11-29 16:55:42 +0800

    Update check_cron_xray.sh

--- check_cron_xray.sh
+++ check_cron_xray.sh
@@ -78,6 +78,8 @@ then
 else
     echo "$PROCESS_NAME 进程正在运行。"
 fi' >> $FILE_NAME
+echo $FILE_NAME
   chmod +x $FILE_NAME
+  
   (crontab -l | grep -F "* * pgrep -x \"xray\" > /dev/null || ${CRON_XRAY}") || (crontab -l; echo "* * * * * pgrep -x \"xray\" > /dev/null || ${CRON_XRAY}") | crontab -
   fi
